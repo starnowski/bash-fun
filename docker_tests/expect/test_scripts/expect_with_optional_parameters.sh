615f25cd15909842f783bdc2bcb1cb41e757e759
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2019-11-09 19:32:56 +0100

    #8 - Added partial implementation. [skip travis]

--- docker_tests/expect/test_scripts/expect_with_optional_parameters.sh
+++ docker_tests/expect/test_scripts/expect_with_optional_parameters.sh
@@ -4,22 +4,29 @@ package require cmdline
 
 set options {
     {a          "set the atime only"}
-    {m          "set the mtime only"}
-    {c          "do not create non-existent files"}
+    {b          "set the mtime only"}
     {r.arg  ""  "use time from ref_file"}
-    {t.arg  -1  "use specified time"}
+    {q.arg  -1  "use specified time"}
     {longOption.arg  "someDefaultValue"  "use string value"}
+    {longOption2.arg  "someDefaultValue"  "use string value"}
 }
 set usage ": MyCommandName \[options] filename ...\noptions:"
 array set params [::cmdline::getoptions argv $options $usage]
 
 
-if {  $params(a) } { set set_atime "true" }
-    set has_t [expr {$params(t) != -1}]
-    set has_r [expr {[string length $params(r)] > 0}]
-if {$has_t && $has_r} {
-    return -code error "Cannot specify both -r and -t"
-} elseif {$has_t} {
-    puts "T option was passed"
-    return 1
+#if {  $params(a) } { set set_atime "true" }
+#    set has_t [expr {$params(t) != -1}]
+#    set has_r [expr {[string length $params(r)] > 0}]
+#if {$has_t && $has_r} {
+#    return -code error "Cannot specify both -r and -t"
+#} elseif {$has_t} {
+#    puts "T option was passed"
+#    return 1
+#}
+
+if {  $params(a) } { puts "Parameter 'a' was passed" }
+if {  $params(b) } {
+    puts "Parameter 'b' was passed"
+} else {
+    puts "Parameter 'b' was not passed"
 }
